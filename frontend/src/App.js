836b22cc7e903da04b3dc66fbe33b0b6d9702959
Greg <gbrewton1980@gmail.com>
2021-10-28 17:02:59 -0600

    style(App Component): Chakra-UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { BASE_URL } from './data';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
+import { ChakraProvider, chakra, Flex, Box } from '@chakra-ui/react';
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -26,22 +27,37 @@ const App = () => {
 
   return (
     <Router>
-      <div data-testid="app-wrapper">
-        <header>
-          <h1>Node API 1 Project</h1>
-        </header>
-        <button>
-          <Link to="/adduser">Add User</Link>
-        </button>
-        <Switch>
-          <Route path="/addUser">
-            <AddUser setUsers={setUsers} />
-          </Route>
-          <Route path="/">
-            <UsersList loading={loading} users={users} setUsers={setUsers} />
-          </Route>
-        </Switch>
-      </div>
+      <ChakraProvider>
+        <div data-testid="app-wrapper">
+          <chakra.header>
+            <Flex align="center" justify="space-between">
+              <Box textAlign="center" w="80%" >
+                <chakra.h1 fontSize={32}>Node API 1 Project</chakra.h1>
+              </Box>
+              <Link to="/adduser">
+                <chakra.button
+                  mr="2"
+                  px="3"
+                  py="2"
+                  bg="green.200"
+                  rounded="md"
+                  _hover={{ bg: 'green.300' }}
+                >
+                  Add User
+                </chakra.button>
+              </Link>
+            </Flex>
+          </chakra.header>
+          <Switch>
+            <Route path="/addUser">
+              <AddUser setUsers={setUsers} />
+            </Route>
+            <Route path="/">
+              <UsersList loading={loading} users={users} setUsers={setUsers} />
+            </Route>
+          </Switch>
+        </div>
+      </ChakraProvider>
     </Router>
   );
 };
